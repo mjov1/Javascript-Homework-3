fbec2e81d3a0023e7324b9c166609a4ebbf66173
Martin Jovanovski <martinjovanovski@Martins-MacBook-Air.local>
2023-02-13 12:41:32 +0100

    added solutuon to exercise 2

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ if (typeof character === 'string') {
 }
 
 // Solution to exercise 1 ends here
+
+// Solution to exercise 2 starts here
+
+let a = 12;
+let b = 10;
+let c = 3;
+
+if (typeof a === 'number' && typeof b === 'number' && typeof c === 'number') {
+    if (a + b > c && a + c > b && b + c > a) {
+        console.log("This is a possible triangle")
+    } else {
+        console.log("This is not a possible triangle")
+    }
+} else {
+    console.log("Invalid variables")
+}
+
+// Solution to exercise 2 ends here
+
+
