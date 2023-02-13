4ae19d1bce4056bf27360c0be7a47bbb60bdefb1
Martin Jovanovski <martinjovanovski@Martins-MacBook-Air.local>
2023-02-13 12:37:09 +0100

    added solution to exercise 1

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// Solution to exercise 1 starts here
+
+let character = "A";
+
+if (typeof character === 'string') {
+    if (character === "a" || character === "A" || character === "e" || character === "E" || character === "i" || character === "I" || character === "o" || character === "O" || character === "u" || character === "U") {
+        console.log("Vowel")
+    } else {
+        console.log("Consonant")
+    }
+} else {
+    console.log("Invalid Character")
+}
